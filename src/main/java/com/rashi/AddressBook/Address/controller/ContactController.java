d146db6eb507c45e50c2ccb757ac2be6140086ea
Rashi123ab <rashia729@gmail.com>
2025-03-06 15:53:55 +0530

    Dependency injection

--- src/main/java/com/rashi/AddressBook/Address/controller/ContactController.java
+++ src/main/java/com/rashi/AddressBook/Address/controller/ContactController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/contact")
-public class ContactController {
+public class ContactController {  //    UC4--using contact service in controller
 
     @Autowired
     private ContactService contactService;
@@ -21,25 +21,21 @@ public class ContactController {
         return ResponseEntity.ok(contactService.getAllContacts());
     }
 
-    // Get contact by ID
     @GetMapping("/{id}")
     public ResponseEntity<Contact> getContactById(@PathVariable Long id) {
         return ResponseEntity.ok(contactService.getContactById(id));
     }
 
-    // Create a new contact
     @PostMapping
     public ResponseEntity<Contact> addContact(@RequestBody Contact contact) {
         return ResponseEntity.ok(contactService.addContact(contact));
     }
 
-    // Update contact by ID
     @PutMapping("/{id}")
     public ResponseEntity<Contact> updateContact(@PathVariable Long id, @RequestBody Contact contact) {
         return ResponseEntity.ok(contactService.updateContact(id, contact));
     }
 
-    // Delete contact by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteContact(@PathVariable Long id) {
         contactService.deleteContact(id);
